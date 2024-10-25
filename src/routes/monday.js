38b8e1feece6a0a9eaf6ec338dc0833f63b159fa
gungavg <velgar.ja@gmail.com>
2024-10-25 11:55:28 -0600

    se agrega funcion de concatenar dos columnas

--- src/routes/monday.js
+++ src/routes/monday.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { authenticationMiddleware } = require('../middlewares/authentication');
 const mondayController = require('../controllers/monday-controller');
+const concatenarController= require('../controllers/concatenar-controller')
 
 router.post('/monday/execute_action', authenticationMiddleware, mondayController.executeAction);
 router.post('/monday/get_remote_list_options', authenticationMiddleware, mondayController.getRemoteListOptions);
+router.post('/monday/execute_concat', authenticationMiddleware, concatenarController.executeAction);
 
 module.exports = router;
